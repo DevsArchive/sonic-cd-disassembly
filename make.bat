b80df871ed6dddd8b9d7798a21138bce1227b548
Ralakimus <uglyduckling623@gmail.com>
2021-11-09 23:36:15 -0500

    oops

--- make.bat
+++ make.bat
@@ -1,4 +1,5 @@
 @echo off
+if not exist _built mkdir _built
 _bin\asm68k.exe /p /o ae- /o l. sp\sp.asm, _built\sp.bin, , sp\sp.lst
 _bin\asm68k.exe /p /o ae- /o l. sp\spx.asm, _built\SPX___.BIN, , sp\spx.lst
 _bin\asm68k.exe /p /o ae- /o l. buram\buramsub.asm, _built\BRAMSUB.BIN, , buram\buramsub.lst
