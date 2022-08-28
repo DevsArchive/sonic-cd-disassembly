1d3a95beda6dc1a09f140a518146918bd1891424
Ralakimus <uglyduckling623@gmail.com>
2022-08-27 21:13:52 -0400

    Split UFO path data

--- Special Stage/Objects/UFO/Main.asm
+++ Special Stage/Objects/UFO/Main.asm
@@ -505,16 +505,10 @@ SpawnTimeUFO:
 ; -------------------------------------------------------------------------
 
 TimeUFOPath:
-	dc.b	2, 0
-	dc.w	$5A, $800, $800, $780, $800
-	dc.w	$5A, $780, $800, $800, $800
-	dc.w	$5A, $800, $800, $800, $780
-	dc.w	$5A, $800, $780, $800, $800
-	dc.w	$5A, $800, $800, $880, $800
-	dc.w	$5A, $880, $800, $800, $800
-	dc.w	$5A, $800, $800, $800, $880
-	dc.w	$5A, $800, $880, $800, $800
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Time UFO Path.bin"
+	even
+
+; -------------------------------------------------------------------------
 
 UFOPaths_SS1:
 	dc.w	6
@@ -588,364 +582,198 @@ UFOPaths_SS8:
 	dc.l	UFOPath_SS8_5
 	dc.l	UFOPath_SS8_6
 
+; -------------------------------------------------------------------------
+
 UFOPath_SS1_1:
-	dc.b	0, 0
-	dc.w	$B4, $780, $B40, $900, $A00
-	dc.w	$78, $900, $A00, $780, $B40
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 1/UFO 1.bin"
+	even
 
 UFOPath_SS1_2:
-	dc.b	0, 0
-	dc.w	$B4, $600, $880, $780, $680
-	dc.w	$78, $780, $680, $580, $780
-	dc.w	$78, $580, $780, $600, $880
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 1/UFO 2.bin"
+	even
 
 UFOPath_SS1_3:
-	dc.b	1, 0
-	dc.w	$B4, $A80, $580, $900, $500
-	dc.w	$78, $900, $500, $980, $680
-	dc.w	$F0, $980, $680, $A80, $580
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 1/UFO 3.bin"
+	even
 
 UFOPath_SS1_4:
-	dc.b	0, 0
-	dc.w	$F0, $780, $480, $580, $500
-	dc.w	$78, $580, $500, $780, $480
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 1/UFO 4.bin"
+	even
 
 UFOPath_SS1_5:
-	dc.b	0, 0
-	dc.w	$3C, $B00, $A00, $B00, $980
-	dc.w	$B4, $B00, $980, $A00, $A00
-	dc.w	$78, $A00, $A00, $A80, $B00
-	dc.w	$78, $A80, $B00, $B00, $A00
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 1/UFO 5.bin"
+	even
 
 UFOPath_SS1_6:
-	dc.b	1, 0
-	dc.w	$B4, $980, $880, $900, $980
-	dc.w	$78, $900, $980, $B00, $880
-	dc.w	$F0, $B00, $880, $980, $880
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 1/UFO 6.bin"
+	even
 
 UFOPath_SS2_1:
-	dc.b	0, 0
-	dc.w	$F0, $B00, $580, $8C0, $4C0
-	dc.w	$F0, $8C0, $4C0, $800, $600
-	dc.w	$F0, $800, $600, $A00, $6C0
-	dc.w	$F0, $A00, $6C0, $B00, $580
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 2/UFO 1.bin"
+	even
 
 UFOPath_SS2_2:
-	dc.b	0, 0
-	dc.w	$B4, $580, $500, $500, $580
-	dc.w	$F0, $500, $580, $680, $680
-	dc.w	$F0, $680, $680, $580, $500
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 2/UFO 2.bin"
+	even
 
 UFOPath_SS2_3:
-	dc.b	1, 0
-	dc.w	$F0, $680, $700, $580, $700
-	dc.w	$12C, $580, $700, $4C0, $800
-	dc.w	$12C, $4C0, $800, $680, $800
-	dc.w	$168, $680, $800, $680, $700
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 2/UFO 3.bin"
+	even
 
 UFOPath_SS2_4:
-	dc.b	0, 0
-	dc.w	$78, $600, $980, $700, $A00
-	dc.w	$3C, $700, $A00, $600, $980
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 2/UFO 4.bin"
+	even
 
 UFOPath_SS2_5:
-	dc.b	0, 0
-	dc.w	$12C, $A00, $900, $840, $AC0
-	dc.w	$12C, $840, $AC0, $A00, $B00
-	dc.w	$F0, $A00, $B00, $A00, $900
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 2/UFO 5.bin"
+	even
 
 UFOPath_SS2_6:
-	dc.b	1, 0
-	dc.w	$F0, $B40, $800, $A00, $780
-	dc.w	$78, $A00, $780, $980, $8C0
-	dc.w	$F0, $980, $8C0, $B40, $800
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 2/UFO 6.bin"
+	even
 
 UFOPath_SS3_1:
-	dc.b	0, 0
-	dc.w	$50, $B00, $500, $A00, $500
-	dc.w	$C8, $A00, $500, $900, $700
-	dc.w	$78, $900, $700, $A00, $680
-	dc.w	$A0, $A00, $680, $B00, $500
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 3/UFO 1.bin"
+	even
 
 UFOPath_SS3_2:
-	dc.b	0, 0
-	dc.w	$C8, $6C0, $4C0, $500, $600
-	dc.w	$A0, $500, $600, $640, $580
-	dc.w	$A0, $640, $580, $6C0, $4C0
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 3/UFO 2.bin"
+	even
 
 UFOPath_SS3_3:
-	dc.b	1, 0
-	dc.w	$78, $600, $780, $500, $780
-	dc.w	$C8, $500, $780, $500, $880
-	dc.w	$A0, $500, $880, $600, $880
-	dc.w	$A0, $600, $880, $600, $780
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 3/UFO 3.bin"
+	even
 
 UFOPath_SS3_4:
-	dc.b	0, 0
-	dc.w	$78, $600, $980, $500, $980
-	dc.w	$C8, $500, $980, $500, $B00
-	dc.w	$78, $500, $B00, $600, $980
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 3/UFO 4.bin"
+	even
 
 UFOPath_SS3_5:
-	dc.b	0, 0
-	dc.w	$C8, $8C0, $A00, $700, $9C0
-	dc.w	$C8, $700, $9C0, $700, $B00
-	dc.w	$A0, $700, $B00, $8C0, $B00
-	dc.w	$A0, $8C0, $B00, $8C0, $A00
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 3/UFO 5.bin"
+	even
 
 UFOPath_SS3_6:
-	dc.b	1, 0
-	dc.w	$C8, $B00, $980, $A00, $880
-	dc.w	$C8, $A00, $880, $980, $A00
-	dc.w	$78, $980, $A00, $980, $B00
-	dc.w	$F0, $980, $B00, $B00, $980
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 3/UFO 6.bin"
+	even
 
 UFOPath_SS4_1:
-	dc.b	0, 0
-	dc.w	$A0, $AC0, $4C0, $880, $500
-	dc.w	$C8, $880, $500, $B00, $600
-	dc.w	$A0, $B00, $600, $AC0, $4C0
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 4/UFO 1.bin"
+	even
 
 UFOPath_SS4_2:
-	dc.b	0, 0
-	dc.w	$C8, $8A0, $5C0, $740, $5C0
-	dc.w	$C8, $740, $5C0, $740, $700
-	dc.w	$C8, $740, $700, $8A0, $700
-	dc.w	$C8, $8A0, $700, $8A0, $5C0
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 4/UFO 2.bin"
+	even
 
 UFOPath_SS4_3:
-	dc.b	1, 0
-	dc.w	$C8, $600, $700, $500, $800
-	dc.w	$C8, $500, $800, $600, $900
-	dc.w	$78, $600, $900, $680, $800
-	dc.w	$78, $680, $800, $600, $700
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 4/UFO 3.bin"
+	even
 
 UFOPath_SS4_4:
-	dc.b	0, 0
-	dc.w	$C8, $600, $A80, $440, $A80
-	dc.w	$A0, $440, $A80, $600, $A80
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 4/UFO 4.bin"
+	even
 
 UFOPath_SS4_5:
-	dc.b	0, 0
-	dc.w	$78, $980, $900, $880, $900
-	dc.w	$A0, $880, $900, $740, $A80
-	dc.w	$C8, $740, $A80, $980, $900
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 4/UFO 5.bin"
+	even
 
 UFOPath_SS4_6:
-	dc.b	1, 0
-	dc.w	$A0, $A80, $6C0, $980, $700
-	dc.w	$A0, $980, $700, $B00, $780
-	dc.w	$78, $B00, $780, $A80, $6C0
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 4/UFO 6.bin"
+	even
 
 UFOPath_SS5_1:
-	dc.b	0, 0
-	dc.w	$14, $B80, $440, $B00, $440
-	dc.w	$50, $B00, $440, $A00, $580
-	dc.w	$50, $A00, $580, $BC0, $580
-	dc.w	$3C, $BC0, $580, $B80, $440
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 5/UFO 1.bin"
+	even
 
 UFOPath_SS5_2:
-	dc.b	0, 0
-	dc.w	$3C, $700, $440, $5C0, $440
-	dc.w	$64, $5C0, $440, $780, $640
-	dc.w	$3C, $780, $640, $840, $580
-	dc.w	$64, $840, $580, $700, $440
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 5/UFO 2.bin"
+	even
 
 UFOPath_SS5_3:
-	dc.b	1, 0
-	dc.w	$64, $AC0, $700, $840, $700
-	dc.w	$64, $840, $700, $AC0, $700
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 5/UFO 3.bin"
+	even
 
 UFOPath_SS5_4:
-	dc.b	0, 0
-	dc.w	$3C, $5C0, $780, $540, $880
-	dc.w	$3C, $540, $880, $500, $940
-	dc.w	$3C, $500, $940, $5C0, $980
-	dc.w	$50, $5C0, $980, $600, $840
-	dc.w	$3C, $600, $840, $5C0, $780
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 5/UFO 4.bin"
+	even
 
 UFOPath_SS5_5:
-	dc.b	0, 0
-	dc.w	$28, $740, $880, $6A0, $900
-	dc.w	$64, $6A0, $900, $6A0, $BC0
-	dc.w	$3C, $6A0, $BC0, $740, $BC0
-	dc.w	$64, $740, $BC0, $740, $880
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 5/UFO 5.bin"
+	even
 
 UFOPath_SS5_6:
-	dc.b	1, 0
-	dc.w	$50, $980, $840, $840, $980
-	dc.w	$64, $840, $980, $AC0, $980
-	dc.w	$50, $AC0, $980, $980, $840
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 5/UFO 6.bin"
+	even
 
 UFOPath_SS6_1:
-	dc.b	0, 0
-	dc.w	$50, $B00, $500, $B00, $680
-	dc.w	$50, $B00, $680, $A80, $7C0
-	dc.w	$3C, $A80, $7C0, $C00, $7C0
-	dc.w	$78, $C00, $7C0, $B00, $500
-	dc.w	$FFFF
-	
+	incbin	"Special Stage/Data/Stage 6/UFO 1.bin"
+	even
+
 UFOPath_SS6_2:
-	dc.b	0, 0
-	dc.w	$64, $980, $480, $7C0, $580
-	dc.w	$64, $7C0, $580, $A00, $580
-	dc.w	$3C, $A00, $580, $980, $480
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 6/UFO 2.bin"
+	even
 
 UFOPath_SS6_3:
-	dc.b	1, 0
-	dc.w	$3C, $4C0, $480, $3C0, $480
-	dc.w	$64, $3C0, $480, $3C0, $600
-	dc.w	$3C, $3C0, $600, $4C0, $600
-	dc.w	$64, $4C0, $600, $4C0, $480
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 6/UFO 3.bin"
+	even
 
 UFOPath_SS6_4:
-	dc.b	0, 0
-	dc.w	$64, $580, $9C0, $400, $B80
-	dc.w	$64, $400, $B80, $680, $A80
-	dc.w	$3C, $680, $A80, $580, $9C0
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 6/UFO 4.bin"
+	even
 
 UFOPath_SS6_5:
-	dc.b	0, 0
-	dc.w	$78, $A00, $940, $600, $940
-	dc.w	$50, $600, $940, $A00, $940
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 6/UFO 5.bin"
+	even
 
 UFOPath_SS6_6:
-	dc.b	1, 0
-	dc.w	$64, $C00, $880, $A80, $880
-	dc.w	$78, $A80, $880, $C00, $B80
-	dc.w	$50, $C00, $B80, $C00, $880
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 6/UFO 6.bin"
+	even
 
 UFOPath_SS7_1:
-	dc.b	0, 0
-	dc.w	$28, $A00, $700, $B00, $600
-	dc.w	$28, $B00, $600, $A00, $700
-	dc.w	$3C, $A00, $700, $A80, $600
-	dc.w	$3C, $A80, $600, $A00, $700
-	dc.w	$50, $A00, $700, $A00, $600
-	dc.w	$3C, $A00, $600, $A00, $700
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 7/UFO 1.bin"
+	even
 
 UFOPath_SS7_2:
-	dc.b	0, 0
-	dc.w	$28, $500, $700, $400, $700
-	dc.w	$50, $400, $700, $500, $940
-	dc.w	$28, $500, $940, $400, $940
-	dc.w	$50, $400, $940, $500, $700
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 7/UFO 2.bin"
+	even
 
 UFOPath_SS7_3:
-	dc.b	1, 0
-	dc.w	$3C, $4C0, $B80, $580, $B00
-	dc.w	$3C, $580, $B00, $4C0, $B80
-	dc.w	$3C, $4C0, $B80, $400, $B00
-	dc.w	$3C, $400, $B00, $4C0, $B80
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 7/UFO 3.bin"
+	even
 
 UFOPath_SS7_4:
-	dc.b	0, 0
-	dc.w	$50, $680, $A00, $680, $B00
-	dc.w	$50, $680, $B00, $680, $A00
-	dc.w	$78, $680, $A00, $880, $B80
-	dc.w	$78, $880, $B80, $680, $A00
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 7/UFO 4.bin"
+	even
 
 UFOPath_SS7_5:
-	dc.b	0, 0
-	dc.w	$50, $A80, $980, $900, $B00
-	dc.w	$64, $900, $B00, $BC0, $BC0
-	dc.w	$64, $BC0, $BC0, $A80, $980
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 7/UFO 5.bin"
+	even
 
 UFOPath_SS7_6:
-	dc.b	1, 0
-	dc.w	$50, $C00, $680, $B00, $800
-	dc.w	$28, $B00, $800, $C00, $800
-	dc.w	$3C, $C00, $800, $C00, $680
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 7/UFO 6.bin"
+	even
 
 UFOPath_SS8_1:
-	dc.b	0, 0
-	dc.w	$78, $B00, $B80, $C00, $980
-	dc.w	$3C, $C00, $980, $C00, $A80
-	dc.w	$50, $C00, $A80, $B00, $B80
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 8/UFO 1.bin"
+	even
 
 UFOPath_SS8_2:
-	dc.b	1, 0
-	dc.w	$78, $C00, $700, $A00, $500
-	dc.w	$50, $A00, $500, $B80, $440
-	dc.w	$78, $B80, $440, $C00, $700
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 8/UFO 2.bin"
+	even
 
 UFOPath_SS8_3:
-	dc.b	0, 0
-	dc.w	$78, $980, $940, $A80, $880
-	dc.w	$78, $A80, $880, $980, $800
-	dc.w	$78, $980, $800, $A80, $780
-	dc.w	$3C, $A80, $780, $A00, $700
-	dc.w	$50, $A00, $700, $A00, $600
-	dc.w	$50, $A00, $600, $A00, $700
-	dc.w	$3C, $A00, $700, $A80, $780
-	dc.w	$78, $A80, $780, $980, $800
-	dc.w	$78, $980, $800, $A80, $880
-	dc.w	$78, $A80, $880, $980, $940
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 8/UFO 3.bin"
+	even
 
 UFOPath_SS8_4:
-	dc.b	0, 0
-	dc.w	$50, $800, $700, $680, $780
-	dc.w	$78, $680, $780, $700, $500
-	dc.w	$50, $700, $500, $580, $580
-	dc.w	$78, $580, $580, $800, $700
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 8/UFO 4.bin"
+	even
 
 UFOPath_SS8_5:
-	dc.b	1, 0
-	dc.w	$50, $680, $B80, $400, $A80
-	dc.w	$3C, $400, $A80, $400, $880
-	dc.w	$3C, $400, $880, $600, $900
-	dc.w	$50, $600, $900, $680, $B80
-	dc.w	$FFFF
-	
+	incbin	"Special Stage/Data/Stage 8/UFO 5.bin"
+	even
+
 UFOPath_SS8_6:
-	dc.b	0, 0
-	dc.w	$50, $600, $480, $400, $600
-	dc.w	$50, $400, $600, $600, $480
-	dc.w	$FFFF
+	incbin	"Special Stage/Data/Stage 8/UFO 6.bin"
+	even
 
 ; -------------------------------------------------------------------------
